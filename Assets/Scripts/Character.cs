79fd5612652510ea170c3f2d23e4870c6b573f26
Adrian Troop <70724439+adrianTroop@users.noreply.github.com>
2024-05-07 09:42:45 +0200

    reset game scene
    Add Character class

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] private int Health;
+    [SerializeField] private float Speed;
+    [SerializeField] private bool isInEndZone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
